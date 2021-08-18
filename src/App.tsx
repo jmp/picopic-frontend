3f73fd3444ae985c8787522b9ded68302cdfe88d
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-08-18 20:50:52 +0300

    Simplify code

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,23 @@ const baseUrl = 'https://api.picopic.io';
 
 async function uploadImage(imageData: ArrayBuffer) {
   console.log('Uploading image.');
+  const response = await fetch(`${baseUrl}/upload-url`, {mode: 'cors'});
+  if (response.status !== 200) {
+    throw new Error('Could not retrieve presigned URL for uploading the image.');
+  }
+  const {url, fields} = await fetchUploadUrl();
+  const formData = constructFormData(imageData, fields);
+  await submitFormData(url, formData);
+  return fields.key;
+}
+
+async function fetchUploadUrl() {
   console.log('Fetching upload URL.');
   const response = await fetch(`${baseUrl}/upload-url`, {mode: 'cors'});
   if (response.status !== 200) {
     throw new Error('Could not retrieve presigned URL for uploading the image.');
   }
-  const {url, fields} = await response.json();
-  console.log('Received upload URL.');
-  const formData = constructFormData(imageData, fields);
-  await submitFormData(url, formData);
-  return fields.key;
+  return await response.json();
 }
 
 function constructFormData(imageData: ArrayBuffer, fields: any): FormData {
@@ -42,13 +49,12 @@ async function fetchDownloadUrl(key: string) {
   if (response.status !== 200) {
     throw new Error(`Could not retrieve download URL (status ${response.status}).`);
   }
-  const {url} = await response.json();
-  return url;
+  return await response.json();
 }
 
 async function downloadImage(key: string) {
   console.log('Downloading image.');
-  const url = await fetchDownloadUrl(key);
+  const {url} = await fetchDownloadUrl(key);
   const response = await fetch(url, {mode: 'cors'});
   if (response.status !== 200) {
     throw new Error(`Image optimization failed (status ${response.status}).`);
@@ -61,58 +67,71 @@ async function processImage(imageData: ArrayBuffer) {
   return await downloadImage(url);
 }
 
+enum State {
+  Ready,
+  Loading,
+  Success,
+  Failure,
+  Aborted,
+}
+
 function Dropzone() {
   const [downloadLink, setDownloadLink] = useState('/');
-  const [isLoading, setLoading] = useState(false);
-  const [isOptimized, setOptimized] = useState(false);
+  const [state, setState] = useState(State.Ready);
   const [originalSize, setOriginalSize] = useState(0);
   const [optimizedSize, setOptimizedSize] = useState(0);
-  const onDrop = useCallback(acceptedFiles => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     console.log('File selected.');
-    setLoading(true);
-    setOptimized(false);
-    const file: File = acceptedFiles[0];
-    const reader = new FileReader();
-    reader.onabort = () => setLoading(false);
-    reader.onerror = reader.onabort;
-    reader.onload = async () => {
-      console.log('Loading started.');
-      const imageData = reader.result;
-      if (!(imageData instanceof ArrayBuffer)) {
-        setLoading(false);
-        console.error('Data is not an ArrayBuffer!');
-        return;
+    setState(State.Loading);
+    acceptedFiles.forEach((file: File) => {
+      const reader = new FileReader();
+      reader.onabort = () => setState(State.Aborted);
+      reader.onerror = () => setState(State.Failure);
+      reader.onload = async () => {
+        console.log('Loading started.');
+        const imageData = reader.result;
+        if (!(imageData instanceof ArrayBuffer)) {
+          setState(State.Failure);
+          console.error('Data is not an ArrayBuffer!');
+          return;
+        }
+        setOriginalSize(imageData.byteLength);
+        console.log('Starting image processing.');
+        try {
+          const optimizedData = await processImage(imageData)
+          const blob = new Blob([optimizedData], {type: 'image/png'});
+          const url = URL.createObjectURL(blob);
+          setOptimizedSize(optimizedData.byteLength);
+          setDownloadLink(url);
+          console.log('Image optimization finished.')
+          setState(State.Success);
+        } catch (error) {
+          console.error('An error occurred while processing image:', error)
+          setState(State.Failure);
+        }
       }
-      setOriginalSize(imageData.byteLength);
-      console.log('Starting image processing.');
-      try {
-        const optimizedData = await processImage(imageData)
-        const blob = new Blob([optimizedData], {type: 'image/png'});
-        const url = URL.createObjectURL(blob);
-        setOptimizedSize(optimizedData.byteLength);
-        setDownloadLink(url);
-        console.log('Image optimization finished.')
-        setOptimized(true);
-      } catch (error) {
-        console.error('An error occurred while processing image:', error)
-      } finally {
-        setLoading(false);
-      }
-    }
-    reader.readAsArrayBuffer(file);
+      reader.readAsArrayBuffer(file);
+    });
   }, []);
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  const {getRootProps, getInputProps} = useDropzone({
+    onDrop,
+    accept: ['image/png'],
+    multiple: false,
+    maxFiles: 1,
+    minSize: 67,
+    maxSize: 5_242_880,
+  })
 
   return (
     <>
-      <div className="loading" hidden={!isLoading}>
+      <div className="loading" hidden={state !== State.Loading}>
         <Loader type="Bars" color="#00BFFF" height={80} width={80} />
       </div>
-      <div className="dropzone" hidden={isLoading} {...getRootProps()}>
+      <div className="dropzone" hidden={state === State.Loading} {...getRootProps()}>
         <input {...getInputProps()} />
-        <p>Drag &amp; drop an image file here to shrink it.</p>
+        <p className="help-text">Drag &amp; drop an image file here to shrink it.</p>
       </div>
-      <div hidden={!isOptimized}>
+      <div hidden={state !== State.Success}>
         <OptimizedImage
           url={downloadLink}
           originalSize={originalSize}
@@ -126,7 +145,7 @@ function Dropzone() {
 type OptimizedImageProps = {
   url: string,
   originalSize: number,
-  optimizedSize: number
+  optimizedSize: number,
 };
 
 function OptimizedImage(props: OptimizedImageProps) {
