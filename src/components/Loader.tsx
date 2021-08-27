64a2636a4360720a7484e4613f453963c0f9568b
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-08-27 17:08:21 +0300

    Remove spinner CSS

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,7 @@ type LoaderProps = {
 export function Loader({hidden}: LoaderProps) {
   return (
     <div hidden={hidden} className="loading">
-      <div title="Loading..." className="lds-spinner">
+      <div title="Loading..." className="spinner">
         <div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/>
       </div>
     </div>
