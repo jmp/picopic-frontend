c6994f16cf15519863792a6443469c84ed02910d
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-03 22:25:54 +0300

    Add hidden test for Error

--- src/components/Error.test.tsx
+++ src/components/Error.test.tsx
@@ -3,9 +3,15 @@ import { render, screen } from '@testing-library/react';
 import {Error} from './Error';
 
 describe('Error', () => {
-  it('renders the given children', () => {
-    const content = 'Testing';
-    render(<Error>{content}</Error>);
-    expect(screen.getByText(content)).toBeInTheDocument();
+  it('shows the given children when not hidden', () => {
+    const content = 'This should be visible';
+    render(<Error hidden={false}>{content}</Error>);
+    expect(screen.getByText(content)).toBeVisible();
+  });
+
+  it('does not show the given children when hidden', () => {
+    const content = 'This should not be visible';
+    render(<Error hidden>{content}</Error>);
+    expect(screen.getByText(content)).not.toBeVisible();
   });
 });
