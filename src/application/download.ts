f3fb51e4b64e146cf7984972ec93d265c85a9f09
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-11 23:45:59 +0300

    Add comments for downloading

--- src/application/download.ts
+++ src/application/download.ts
@@ -1,5 +1,10 @@
 import {baseUrl} from '../../package.json';
 
+/**
+ * Download the image using the given key.
+ *
+ * The same key used for uploading the unoptimized image should be used.
+ */
 export async function downloadImage(key: string): Promise<ArrayBuffer> {
   const {url} = await fetchDownloadUrl(key);
   const response = await fetch(url);
@@ -9,6 +14,7 @@ export async function downloadImage(key: string): Promise<ArrayBuffer> {
   return await response.arrayBuffer();
 }
 
+/** Fetch a download URL for the given key. */
 async function fetchDownloadUrl(key: string): Promise<any> {
   const response = await fetch(`${baseUrl}/download-url/${key}`);
   if (response.status !== 200) {
