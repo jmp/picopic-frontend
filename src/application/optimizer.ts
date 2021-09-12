dfe7ec47d508593cc8bc27d6d9596053fb702837
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-12 12:18:52 +0300

    Add comments for optimizer

--- src/application/optimizer.ts
+++ src/application/optimizer.ts
@@ -4,12 +4,21 @@ export type OptimizationResult = {
   optimizedSize: number,
 };
 
+/** An optimizer class that encapsulates the image optimization logic. */
 export class Optimizer {
   constructor(
     private uploadImage: (imageData: ArrayBuffer) => Promise<string>,
     private downloadImage: (key: string) => Promise<ArrayBuffer>,
   ) {}
 
+  /**
+   * Optimize the given image file.
+   *
+   * The image is optimized by uploading it to the backend for optimization.
+   * The backend will provide an URL that can be used to download an optimized
+   * version of the same image. The optimized image will be downloaded, and
+   * the optimizer will return a result with some statistics and a download URL.
+   */
   async optimize(file: File): Promise<OptimizationResult> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
